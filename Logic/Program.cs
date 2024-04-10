1febb0ab234791b7316549e721971117a7b62ee8
248677 Kovalenko Yehor <248677@edu.p.lodz.pl>
2024-04-10 21:03:16 +0200

    Layers created

--- Logic/Program.cs
+++ Logic/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,3 +18,4 @@ namespace Project
         }
     }
 }
+*/
