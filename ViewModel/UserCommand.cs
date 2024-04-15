5c6732ee156fb240f7ba31c8e8fcb6c89397e346
248677 Kovalenko Yehor <248677@edu.p.lodz.pl>
2024-04-15 23:40:11 +0200

    Binding v0.0

---
+++ ViewModel/UserCommand.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    internal class UserCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
