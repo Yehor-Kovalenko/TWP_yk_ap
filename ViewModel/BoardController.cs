2b741e8ccaacea9614d36a3ff10bad4c56514514
248677 Kovalenko Yehor <248677@edu.p.lodz.pl>
2024-04-16 01:08:53 +0200

    Binding v0.2 created MainWindowController and started work on ModelManager

--- ViewModel/BoardController.cs
+++ ViewModel/BoardController.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace ViewModel
 {
-    internal class ViewModelController
+    internal class BoardController
     {
-       
     }
 }
---
+++ ViewModel/MainWindowController.cs
@@ -0,0 +1,33 @@
+﻿
+namespace ViewModel
+{
+    internal class MainWindowController
+    {
+        private UserCommand commandGenerate;
+        private UserCommand commandSlider;
+
+        //logicAPI z modelu//
+
+        public UserCommand CommandGenerate { get => commandGenerate; set => commandGenerate = value; }
+        public UserCommand CommandSlider { get => commandSlider; set => commandSlider = value; }
+        //property na modelapi//
+        //collection of balls//
+
+        public MainWindowController()
+        {
+            CommandGenerate = new UserCommand(Start, StartStatus);
+            CommandSlider = new UserCommand(Create, CreateStatus);
+            //modelAPi instantiate//
+        }
+
+        private bool StartStatus()
+        {
+            return true;
+        }
+
+        private bool CreateStatus()
+        {
+            return true;
+        }
+    }
+}
