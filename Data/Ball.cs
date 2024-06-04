f9b87543f532e7f48c2f66095911c3ca527a9d76
248677 Kovalenko Yehor <248677@edu.p.lodz.pl>
2024-06-05 01:58:55 +0200

    fix: ball fixed

--- Data/Ball.cs
+++ Data/Ball.cs
@@ -5,16 +5,19 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Threading;
 
 namespace Data
 {
     public class Ball
     {
-        public Vector2 Speed { get; set; }
-        public Vector2 Position { get; set; }
+        private Vector2 position;
+        private Vector2 speed;
         public double Radius { get; set; }
 
         public int Mass { get; set; }
+
+        private static object _lockObject = new object();
 
         public Ball(Vector2 k, double pr, Vector2 p)
         {
@@ -24,6 +27,43 @@ namespace Data
             Speed = p;
             Mass = r.Next(1, 10);
             Debug.WriteLine($"Kula zostala utworzona");
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+               lock (_lockObject)
+                {
+                    return position;
+                }
+
+            }
+            set
+            {
+                lock (_lockObject)
+                {
+                    position = value;
+                }
+            }
+        }
+        public Vector2 Speed
+        {
+            get
+            {
+                lock (_lockObject)
+                {
+                    return speed;
+                }
+
+            }
+            set
+            {
+                lock (_lockObject)
+                {
+                    speed = value;
+                }
+            }
         }
     }
 }
